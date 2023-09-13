b4a8e53d8ffe8753280d77cb50c6b07991889e16
scian0204 <scian0204@gmail.com>
2023-09-13 10:43:10 +0900

    Adding modal functionality
    바깥부분 터치 시 모달 비활성화 기능 추가

--- components/TicketModal.tsx
+++ components/TicketModal.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Modal, StyleSheet, Text, View } from 'react-native';
+import { Modal, Pressable, StyleSheet, Text, View } from 'react-native';
 
 interface propsIntf {
   ticketModalVisible: boolean;
@@ -9,11 +9,28 @@ interface propsIntf {
 function TicketModal({ ticketModalVisible, toggleTicketModal }: propsIntf) {
   const styles = StyleSheet.create({
     modalView: {
-      marginBottom: '15%',
+      marginBottom: '20%',
       backgroundColor: 'white',
       borderRadius: 20,
       padding: 35,
       alignItems: 'center',
+      shadowColor: '#000',
+      shadowOffset: {
+        width: 0,
+        height: 2,
+      },
+      shadowOpacity: 0.25,
+      shadowRadius: 4,
+      elevation: 5,
+    },
+    ticketModalView: {
+      marginBottom: '10%',
+      backgroundColor: 'white',
+      borderRadius: 20,
+      width: '80%',
+      height: '50%',
+      alignItems: 'center',
+      justifyContent: 'center',
       shadowColor: '#000',
       shadowOffset: {
         width: 0,
@@ -37,6 +54,16 @@ function TicketModal({ ticketModalVisible, toggleTicketModal }: propsIntf) {
           justifyContent: 'flex-end',
           alignItems: 'center',
         }}>
+        <Pressable
+          onPress={toggleTicketModal}
+          style={{
+            position: 'absolute',
+            width: '100%',
+            height: '100%',
+          }}></Pressable>
+        <View style={styles.ticketModalView}>
+          <Text>Ticket</Text>
+        </View>
         <View style={styles.modalView}>
           <Text onPress={toggleTicketModal}>Close Modal</Text>
         </View>
